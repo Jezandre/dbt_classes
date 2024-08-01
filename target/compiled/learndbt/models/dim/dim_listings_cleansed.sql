23ac1f4d76b85eca5f88d890a7dc15eb1daabd46
jezandre <jezandre_tiago@hotmail.com>
2024-08-01 05:53:45 -0300

    Atualizando

--- target/compiled/learndbt/models/dim/dim_listings_cleansed.sql
+++ target/compiled/learndbt/models/dim/dim_listings_cleansed.sql
@@ -1,22 +1,22 @@
 
 WITH  __dbt__cte__src_listings as (
-WITH raw_listings AS (
-    SELECT
-    *
-    FROM
-    AIRBNB.RAW.RAW_LISTINGS
-)
-SELECT
-    id AS listing_id,
-    name AS listing_name,
-    listing_url,
-    room_type,
-    minimum_nights,
-    host_id,
-    price AS price_str,
-    created_at,
-    updated_at
-FROM
+WITH raw_listings AS (
+    SELECT
+    *
+    FROM
+     AIRBNB.raw.raw_listings
+)
+SELECT
+    id AS listing_id,
+    name AS listing_name,
+    listing_url,
+    room_type,
+    minimum_nights,
+    host_id,
+    price AS price_str,
+    created_at,
+    updated_at
+FROM
     raw_listings
 ), src_listings AS (
     SELECT * FROM __dbt__cte__src_listings
