75843b6f48c26333feb07c0bd8b51f5f312efd15
jezandre <jezandre_tiago@hotmail.com>
2024-08-02 05:30:30 -0300

    Nova atualização

--- models/fct/fct_reviews.sql
+++ models/fct/fct_reviews.sql
@@ -8,7 +8,9 @@
 WITH src_reviews AS (
  SELECT * FROM {{ ref('src_reviews') }}
 )
-SELECT * FROM src_reviews
+SELECT 
+{{ dbt_utils.generate_surrogate_key(['listing_id', 'review_date', 'reviewer_name', 'review_text']) }} as review_id,
+* FROM src_reviews
 WHERE REVIEW_TEXT is not null
 {% if is_incremental() %}
  AND review_date > (select max(review_date) from {{ this }})
--- target/compiled/learndbt/models/fct/fct_reviews.sql
+++ target/compiled/learndbt/models/fct/fct_reviews.sql
@@ -18,7 +18,7 @@ FROM raw_reviews
 ), src_reviews AS (
  SELECT * FROM __dbt__cte__src_reviews
 )
-SELECT * FROM src_reviews
+SELECT 
+md5(cast(coalesce(cast(listing_id as TEXT), '_dbt_utils_surrogate_key_null_') || '-' || coalesce(cast(review_date as TEXT), '_dbt_utils_surrogate_key_null_') || '-' || coalesce(cast(reviewer_name as TEXT), '_dbt_utils_surrogate_key_null_') || '-' || coalesce(cast(review_text as TEXT), '_dbt_utils_surrogate_key_null_') as TEXT)) as review_id,
+* FROM src_reviews
 WHERE REVIEW_TEXT is not null
-
- AND review_date > (select max(review_date) from AIRBNB.DEV.fct_reviews)
