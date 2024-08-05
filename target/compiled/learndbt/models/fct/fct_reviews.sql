933eb846f4f02c4a66b0b8215950dee3bfae75ac
jezandre <jezandre_tiago@hotmail.com>
2024-08-05 05:26:23 -0300

    New adds

--- target/compiled/learndbt/models/fct/fct_reviews.sql
+++ target/compiled/learndbt/models/fct/fct_reviews.sql
@@ -22,3 +22,5 @@ SELECT
 md5(cast(coalesce(cast(listing_id as TEXT), '_dbt_utils_surrogate_key_null_') || '-' || coalesce(cast(review_date as TEXT), '_dbt_utils_surrogate_key_null_') || '-' || coalesce(cast(reviewer_name as TEXT), '_dbt_utils_surrogate_key_null_') || '-' || coalesce(cast(review_text as TEXT), '_dbt_utils_surrogate_key_null_') as TEXT)) as review_id,
 * FROM src_reviews
 WHERE REVIEW_TEXT is not null
+
+ AND review_date > (select max(review_date) from AIRBNB.DEV.fct_reviews)
